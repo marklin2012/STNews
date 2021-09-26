673c0ae723504c2b08813e9ae5c49bb0de6374ad
Harry <MingChenXX@163.com>
2021-09-26 23:31:18 +0800

    fix:修改手机号限制的问题

--- lib/utils/phone_input_formatter.dart
+++ lib/utils/phone_input_formatter.dart
@@ -15,12 +15,28 @@ class PhoneInputFormatter extends TextInputFormatter {
             extentOffset: newValue.selection.extentOffset + 1,
           ),
         );
+      } else if (oldValue.text.length == 3) {
+        return TextEditingValue(
+          text: oldValue.text + " " + newValue.text[newValue.text.length - 1],
+          selection: TextSelection(
+            baseOffset: oldValue.selection.baseOffset + 2,
+            extentOffset: oldValue.selection.extentOffset + 2,
+          ),
+        );
       } else if (newValue.text.length == 8) {
         return TextEditingValue(
           text: newValue.text + " ",
           selection: TextSelection(
             baseOffset: newValue.selection.baseOffset + 1,
             extentOffset: newValue.selection.extentOffset + 1,
+          ),
+        );
+      } else if (oldValue.text.length == 8) {
+        return TextEditingValue(
+          text: oldValue.text + " " + newValue.text[newValue.text.length - 1],
+          selection: TextSelection(
+            baseOffset: oldValue.selection.baseOffset + 2,
+            extentOffset: oldValue.selection.extentOffset + 2,
           ),
         );
       }
