76469fc81d576498555950ae8e87944c1b4b0c3d
Mark Lin <marklin@Lenny.lan>
2021-09-28 22:31:17 +0800

    feat: 添加web内容页面展示

--- lib/pages/home/post_detail_page.dart
+++ lib/pages/home/post_detail_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:provider/provider.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:saturn/saturn.dart';
 
 import 'package:stnews/models/post_model.dart';
@@ -111,9 +111,17 @@ class _PostDetailPageState extends State<PostDetailPage> {
                   ),
                   SliverToBoxAdapter(
                     child: Container(
-                      height: 500,
-                      child: Center(
-                        child: Text('WebView'),
+                      child: Html(
+                        data: postDetP.postModel.article,
+                        style: {
+                          'p': Style(
+                            fontSize: FontSize.large,
+                            lineHeight: LineHeight(1.4),
+                          ),
+                        },
+                        onImageError: (err, _) {
+                          print(err);
+                        },
                       ),
                     ),
                   ),
