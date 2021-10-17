2d59ed47be45b538fc70ad61276ead745e311fb7
Harry <MingChenXX@163.com>
2021-10-17 09:53:09 +0800

    fix:修改个人主页头部点击效果问题

--- lib/pages/common/person_home_header.dart
+++ lib/pages/common/person_home_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:stnews/pages/common/color_config.dart';
 
 class PersonHomeHeader extends StatelessWidget {
   const PersonHomeHeader({
@@ -20,35 +19,25 @@ class PersonHomeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(16, 7, 16, 7),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: InkWell(
-              highlightColor: ColorConfig.baseFirBule,
-              onTap: onTap,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  if (leading != null) leading!,
-                  if (leading != null) SizedBox(width: 12.0),
-                  if (title != null) title!,
-                  Expanded(
-                    child: SizedBox(),
-                  ),
-                ],
-              ),
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.fromLTRB(16, 7, 16, 7),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                if (leading != null) leading!,
+                if (leading != null) SizedBox(width: 12.0),
+                if (title != null) title!,
+              ],
             ),
-          ),
-          if (trailing != null)
-            GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTap: onTap,
-              child: trailing!,
-            ),
-        ],
+            if (trailing != null) trailing!,
+          ],
+        ),
       ),
     );
   }
